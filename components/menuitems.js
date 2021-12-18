d1967e27c083101328be0db46fb76a4ec318d26a
Dan-Electro <74511348+Votha-Chheng@users.noreply.github.com>
2021-12-18 17:50:57 +0100

    Fixing bug

--- components/menuitems.js
+++ components/menuitems.js
@@ -16,73 +16,77 @@ const MenuItems = ({bandeauInView}) => {
   const {pathname} = history
 
   useEffect(()=>{
-    setLogoWidth(logoMini.current.clientWidth)
-    setFirstMenuItemX(firstMenuItem.current.offsetLeft)
+    setTimeout(()=> {
+      setLogoWidth(logoMini.current.clientWidth)
+      setFirstMenuItemX(firstMenuItem.current.offsetLeft)
+    },500)
+    
+  }, [])
 
-  }, [logoWidth, firstMenuItemX])
+  return (  
+    <NavStyle>
+      <>
+        <motion.div
+          className="menuInfo" 
+          style={{overflow:"hidden"}}
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          transition={{duration:0.25, delay:1.1, ease:"easeOut" }}
+        >
+          <ul className="menu-items" style={{transform:`translateX(-${ !bandeauInView ? 0 : firstMenuItemX}px)`}}>
+            <li className="logo-item" ref={logoMini}>
+              <Link href="/">
+                <a><img src="/images/logo.svg" width="125" /></a>
+              </Link>
+            </li> 
+            <li ref={firstMenuItem}>
+              <Link href="/soins">
+                <a>Nos soins</a>
+              </Link>
+              <div 
+                className={pathname === "/soins" ? "active" : ""}
+                style={{transform: `translateX(${pathname === "/soins" ? 0: -100}%)`}}
+              />
+            </li>
+            <li>
+              <Link href="/hygiene">
+                <a>Hygiène dentaire</a>
+              </Link>
+              <div 
+                className={pathname === "/hygiene" ? "active" : ""}
+                style={{transform: `translateX(${pathname === "/hygiene" ? 0: -100}%)`}}
+              />
+            </li>
+            <li>
+              <Link href="/urgences">
+                <a>Urgences</a>
+              </Link>
+              <div 
+                className={pathname === "/urgences" ? "active" : ""}
+                style={{transform: `translateX(${pathname === "/urgences" ? 0: -100}%)`}}
+              />
+            </li>
+            <li>
+              <Link href="/infos">
+                <a>Infos pratiques</a>
+              </Link>
+              <div 
+                className={pathname === "/infos"? "active" : ""}
+                style={{transform: `translateX(${pathname === "/infos" ? 0: -100}%)`}}
+              />
+            </li>
+          </ul>
+        </motion.div>
 
-  return (   
-    <NavStyle>   
-      <motion.div
-        className="menuInfo" 
-        style={{overflow:"hidden"}}
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:0.25, delay:1.1, ease:"easeOut" }}
-      >
-        <ul className="menu-items" style={{transform:`translateX(-${ !bandeauInView ? 0 : firstMenuItemX}px)`}}>
-          <li className="logo-item" ref={logoMini}>
-            <Link href="/">
-              <a><img src="/images/logo.svg" width="125" /></a>
-            </Link>
-          </li> 
-          <li ref={firstMenuItem}>
-            <Link href="/soins">
-              <a>Nos soins</a>
-            </Link>
-            <div 
-              className={pathname === "/soins" ? "active" : ""}
-              style={{transform: `translateX(${pathname === "/soins" ? 0: -100}%)`}}
-            />
-          </li>
-          <li>
-            <Link href="/hygiene">
-              <a>Hygiène dentaire</a>
-            </Link>
-            <div 
-              className={pathname === "/hygiene" ? "active" : ""}
-              style={{transform: `translateX(${pathname === "/hygiene" ? 0: -100}%)`}}
-            />
-          </li>
-          <li>
-            <Link href="/urgences">
-              <a>Urgences</a>
-            </Link>
-            <div 
-              className={pathname === "/urgences" ? "active" : ""}
-              style={{transform: `translateX(${pathname === "/urgences" ? 0: -100}%)`}}
-            />
-          </li>
-          <li>
-            <Link href="/infos">
-              <a>Infos pratiques</a>
-            </Link>
-            <div 
-              className={pathname === "/infos"? "active" : ""}
-              style={{transform: `translateX(${pathname === "/infos" ? 0: -100}%)`}}
-            />
-          </li>
-        </ul>
-      </motion.div>
-
-      <motion.div 
-        style={{overflow:"hidden", width:"100%"}}
-        initial={{x:"-100%"}}
-        animate={{x:0}}
-        transition={{duration:0.5, delay:0.5, ease:"easeOut" }}
-      >
-        <LineThrough backgroundColor="var(--main-color-text)"/>
-      </motion.div>  
+        <motion.div 
+          style={{overflow:"hidden", width:"100%"}}
+          initial={{x:"-100%"}}
+          animate={{x:0}}
+          transition={{duration:0.5, delay:0.5, ease:"easeOut" }}
+        >
+          <LineThrough backgroundColor="var(--main-color-text)"/>
+        </motion.div>  
+      </>     
     </NavStyle>
   );
 }
