8f1b3035247e6a70de9b2497c567ae393d06f231
Votha-Chheng <votha_chheng@gmx.fr>
2022-02-21 22:22:13 +0100

    Dernières corrections

--- components/scrollimagemodalresponsive.js
+++ components/scrollimagemodalresponsive.js
@@ -4,20 +4,32 @@ import styled from "styled-components";
 
 const scrollimagemodalresponsive = ({listeImages, selectedImage}) => {
   const [scrolling, setScrolling] = useState(0)
-  const [touchStartX, setTouchStartX] = useState(0)
-  const [start, setStart] = useState(0)
   const [touchStart, setTouchStart] = useState(0)
   const [touchEnd, setTouchEnd] = useState(0)
-  const [translationX, setTranslationX] = useState(null)
+  const [narrowScreen, setNarrowScreen] = useState(false)
+
 
   useEffect(()=>{
     setHorizontalScroll(selectedImage)
     
   }, [selectedImage])
 
-  // useEffect(()=>{
-  //   console.log(Math.floor(568/100)*100)
-  // }, [scrolling])
+  useEffect(()=>{
+    const mediaWatcher = window.matchMedia("(max-width: 500px)")
+    setNarrowScreen(mediaWatcher.matches);
+
+    //watch for updates
+    function updateIsNarrowScreen(event) {
+      setNarrowScreen(event.matches);
+    }
+    mediaWatcher.addEventListener('change', updateIsNarrowScreen)
+
+    // clean up after ourselves
+    return function cleanup() {
+      mediaWatcher.removeEventListener('change', updateIsNarrowScreen)
+    }
+
+  }, [])
 
   const setHorizontalScroll = (selectedImage)=> {
     for(let i=0 ; i<listeImages.length ; i++){
@@ -33,27 +45,31 @@ const scrollimagemodalresponsive = ({listeImages, selectedImage}) => {
   }
   
   const handleTouchMove = (event)=>{
-
     setTouchEnd(event.targetTouches[0].clientX)
-
 
   }
 
-  const handleTouchEnd = ()=>{
-    let XRight = Math.floor(((scrolling/100)*100))-500
-    let XLeft = Math.ceil((scrolling/100)*100)+500
+  const handleTouchEnd = (narrowScr)=>{
+    let size
 
+    if(narrowScr){
+      size = 360
+    }else {
+      size = 500
+    }
+
+    let XRight = Math.floor(((scrolling/100)*100))-size
+    let XLeft = Math.ceil((scrolling/100)*100)+size
+    
     if (touchStart - touchEnd >  0) {
-      if(XLeft>2500){
-        setScrolling(2500)
+      if(XLeft>size*5){
+        setScrolling(size*5)
 
       } else {
         setScrolling(XLeft)
 
-      }
-      
+      }  
     } else {
-      console.log()
       if(XRight<0){
         setScrolling(0)
         
@@ -61,15 +77,9 @@ const scrollimagemodalresponsive = ({listeImages, selectedImage}) => {
         setScrolling(XRight)
 
       }
-
     }
-
-    // if(translationX>3000){
-    //   setTranslationX(3000)
-    // } else if (translationX<0){
-    //   setTranslationX(0)
-    // }
   }
+
 
   return (
     <ScrollContainer >
@@ -77,7 +87,7 @@ const scrollimagemodalresponsive = ({listeImages, selectedImage}) => {
         className="scroller-responsive" 
         style={{transform:`translateX(${-scrolling}px)`}}
         onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
+        onTouchEnd={()=>handleTouchEnd(narrowScreen)}
         onTouchMove={handleTouchMove}
       >
         {
@@ -96,6 +106,13 @@ const scrollimagemodalresponsive = ({listeImages, selectedImage}) => {
 const ScrollContainer = styled.div`
   width:100%;
 
+  .img-container-res-500{
+    min-width:360px;
+    height:80vh;
+    display:none;
+    position:relative;
+  }
+
   .scroller-responsive {
     display:flex;
     flex-direction:row;
@@ -106,6 +123,14 @@ const ScrollContainer = styled.div`
     min-width:500px;
     height:90vh;
     position:relative;
+    display:block
+  }
+
+  @media (max-width:500px){
+    .img-container-res{
+      min-width:360px;
+    }
+    
   }
 `
 
