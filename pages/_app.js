ce83738a785c5ff96bab6407cf9408e3f1f87d31
Votha-Chheng <votha_chheng@gmx.fr>
2022-03-18 09:51:48 +0100

    Ajout d'une sitemap

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Soins dentaires, esthétique du sourire et green dentistry sur Montpellier et sa région : bienvenue sur le site du cabinet dentaire L'Envolée"/>
         <meta property="og:title" content="Cabinet dentaire L'Envolée du Dr Sylvie Ma-Francin à Castelnau-le-Lez" />
         <meta property="og:image" content="https://zupimages.net/up/22/07/zoag.png"/>
-        <meta property="og:image:type" content="image/jpg"/>
+        <meta property="og:image:type" content="image/png"/>
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="fr" />
         <link rel="stylesheet/css" href='/global.css'/>
